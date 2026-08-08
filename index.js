15be1d001a481385029c859ef266fcbef7cd7dc8
a <102840087+kingdudely@users.noreply.github.com>
2026-08-08 12:08:13 -0400

    d

--- index.js
+++ index.js
@@ -65,7 +65,6 @@ document.getElementById("start-runner").addEventListener("submit", async (event)
 	if (!ready) return; // gate screen is now showing, or redirect is happening
 
 	const accessToken = (await cookieStore.get("access_token"))?.value;
-	mainDialog.close();
 
 	const formData = new FormData(event.target);
 	const os = formData.get("os");
